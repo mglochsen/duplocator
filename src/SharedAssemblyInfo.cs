8dcecd27391b671826e435de36c7475d7476c961
Matthias Glauch <matthias.glauch@mgsd.eu>
2020-01-29 11:48:40 +0100

    simplifed SharedAssemblyInfo

--- src/SharedAssemblyInfo.cs
+++ src/SharedAssemblyInfo.cs
@@ -3,5 +3,3 @@ using System.Reflection;
 [assembly: AssemblyCopyright("Copyright (C) 2020 Matthias Glauch")]
 
 [assembly: AssemblyVersion("0.9.9")]
-[assembly: AssemblyFileVersion("0.9.9")]
-[assembly: AssemblyInformationalVersion("0.9.9")]
